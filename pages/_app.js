a27fedd231057e28f28a10c3842efeacfb4eadca
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-30 05:15:05 +0600

    outfit font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={outfit.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
