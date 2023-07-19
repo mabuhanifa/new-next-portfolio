dd07b13752378e9c198b59ddb50595d1839d9509
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-19 18:51:39 +0600

    products json updating

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,10 @@
 export default function Main() {
   return (
-    <div className="px-10 md:px-20 flex justify-center bg-[#0F0F0F]">
-      <div className="grad h-40 w-80 p-10 mt-20 rounded-[35px]">Main</div>
+    <div className="px-10 md:px-20 flex justify-center bg-[#0F0F0F] items-center ">
+      <div className="grad h-80 w-[500px] p-10 mt-20 rounded-[30px] ">
+        
+        
+        </div>
     </div>
   );
 }
